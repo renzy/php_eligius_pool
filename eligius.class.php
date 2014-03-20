87b1bd095bac2ddb002f7fc86b37ebddd345e233
Aaron Renzelmann <arenzy@gmail.com>
2014-03-20 01:36:23 -0500

    init commit

--- eligius.class.php
+++ eligius.class.php
@@ -11,11 +11,11 @@ class eligius {
 
 	public function fetch($url,$json_decode=true){
 		$ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL, $url); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        $data = curl_exec($ch); 
-        curl_close($ch);    
-        return ($json_decode) ? json_decode($data) : $data;		
+        	curl_setopt($ch, CURLOPT_URL, $url); 
+        	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        	$data = curl_exec($ch); 
+        	curl_close($ch);    
+        	return ($json_decode) ? json_decode($data) : $data;		
 	}
 
 	//--api based requests
